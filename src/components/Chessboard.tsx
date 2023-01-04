91fddee7ffac78d7c2ba230db93dda8edeb3cb52
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2023-01-03 19:59:13 -0800

    Create an 8x8 checkered board

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Chessboard.css";
 
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
 function Chessboard() {
-  return <div>Chessboard</div>
+  let board = [];
+
+  for(let j = verticalAxis.length - 1; j >= 0; j--) {
+    for(let i = 0; i < horizontalAxis.length; i++) {
+      const squareNum = j + i + 2; // Add 2 since indices start at 0
+
+      if(squareNum % 2 === 0) {
+        board.push(
+          <div className="coordinate dark-square"></div>
+      )}
+
+      else {
+        board.push(
+          <div className="coordinate light-square"></div>
+      )}
+    }
+  }
+
+  console.log("Chessboard", board);
+
+  return <div className="Chessboard">{board}</div>
 }
 
 export default Chessboard;
